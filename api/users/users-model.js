647e4db92d39bbf125f273c41f449da7aeb19c7c
paulstgermain <paulg.stgermain@gmail.com>
2021-03-22 16:09:05 -0400

    fleshes out users-model & users-router

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,29 +1,45 @@
+const db = require('../../data/db-config');
+
 /**
   resolves to an ARRAY with all users, each user having { user_id, username }
  */
 function find() {
-
+  return db('users').select('user_id', 'username');
 }
 
 /**
   resolves to an ARRAY with all users that match the filter condition
  */
 function findBy(filter) {
-
+  return db('users')
+    .where('username', filter);
 }
 
 /**
   resolves to the user { user_id, username } with the given user_id
  */
 function findById(user_id) {
-
+  return db('users')
+    .where('user_id', user_id)
+    .first();
 }
 
 /**
   resolves to the newly inserted user { user_id, username }
  */
 function add(user) {
-
+  return db('users')
+    .insert(user)
+    .then(newUser => {
+      return findBy(newUser.username);
+    });
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+
+module.exports = {
+  find,
+  findBy,
+  findById,
+  add
+};
